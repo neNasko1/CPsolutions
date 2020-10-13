4adb81221bce76f9c3d2bcefbb024fa8b859a0b0
aDimitrov <nasko119@abv.bg>
2020-10-14 02:14:45 +0300

    local

--- OI/IOI/IOI16aliens.cpp
+++ OI/IOI/IOI16aliens.cpp
@@ -10,70 +10,47 @@ template<class T, class T2> inline ostream &operator <<(ostream &out, const pair
 template<class T, class T2> inline istream &operator >>(istream &in, pair<T, T2> &x) { in >> x.first >> x.second; return in;}
 template<class T, class T2> inline bool chkmax(T &x, const T2 &y) { return x < y ? x = y, 1 : 0; }
 template<class T, class T2> inline bool chkmin(T &x, const T2 &y) { return x > y ? x = y, 1 : 0; }
-const ll mod = 1e12 + 7, MAX_CORD = 1e6 + 10;
+const ld mod = 1e12 + 7;
+const ll MAX_CORD = 1e6 + 10;
 const ld eps = 1e-2;
 #define out(x) "{" << (#x) << ": " << x << "} "
 
 struct Line {
-    ld a, b;
-    ll delta;
-    ld y(ld x) {return x * a + b;}
-    Line(ld _a, ld _b, ll _delta) {a = _a; b = _b; delta = _delta;}
-    Line() {a = 0; b = mod; delta = 0;}
-};
-struct Node {
-    Line val;
-    Node *l, *r;
-    Node() {l = nullptr; r = nullptr;}
-    ld y(ld x) {return val.y(x);}
-    void expand() {
-        if(!l) {l = new Node(); r = new Node();}
-    }
-};
-struct LiChao {
-    Node root;
-    void add(Line nw, Node *curr, ll l = 0, ll r = MAX_CORD) {
-        while(true) {
-            ll m = (l + r) / 2;
-            bool a = nw.y(l) < curr->y(l);
-            bool b = nw.y(m) < curr->y(m);
-            if(b) {swap(curr->val, nw);}
-            if(r - l == 1) {
-                return;
-            }
-            curr->expand();
-            if(a != b) {
-                curr = curr->l;
-                r = m;
-            } else {
-                curr = curr->r;
-                l = m;
-            }
+    ld a, b, delta;
+    long double p;
+    Line() {}
+    Line(ld _a, ld _b, ld _delta) {a = _a; b = _b; delta = _delta;}
+    ld y(ld x) {return a * x + b;}
+    long double getWhere(const Line &other) {
+        if(a == other.a) {
+            return -mod;
+        } else {
+            return (long double)(b - other.b) / (long double)(other.a - a);
         }
     }
-    Line get(ll x, Node *curr) {
-        ll l = 0, r = MAX_CORD;
-        Line ret = Line();
-        while(curr != nullptr) {
-            ll m = (l + r) / 2;
-            if(curr->y(x) < ret.y(x)) {
-                ret = curr->val;
-            }
-            if(r - l == 1) {
-                return ret;
-            }
-            if(x < m) {
-                curr = curr->l;
-                r = m;
-            } else {
-                curr = curr->r;
-                l = m;
-            }
+};
+
+struct LineContainer {
+    deque<Line> lines;
+    void addLine(Line curr) {
+        while(!lines.empty() && lines.back().getWhere(curr) <= lines.back().p) {
+            lines.pop_back();
         }
-        return ret;
+        if(lines.empty()) {
+            curr.p = -mod;
+        } else {
+            curr.p = lines.back().getWhere(curr);
+        }
+        lines.push_back(curr);
+    }
+    pair<ld, int> get(ll x) {
+        while(lines.size() > 1 && lines[1].p <= x) {
+            lines.pop_front();
+        }
+        return {lines.front().y(x), lines.front().delta};
     }
     void clear() {
-        root = Node();
+        while(!lines.empty()) {lines.pop_back();}
     }
 };
 
@@ -82,23 +59,24 @@ ll n, m, k;
 vector<pair<ll, ll> > points;
 ld dp[MAX_N];
 ll delta[MAX_N];
-LiChao tree;
+LineContainer lc;
 
 ll toSqr(ll x) {
     return x * x;
 }
 
 ll calculate(ld del) {
-    tree.clear();
+    lc.clear();
     dp[0] = delta[0] = 0;
-    tree.add(Line(-2ll * points[1].second, points[1].second * points[1].second + del, 0), &(tree.root));
+    lc.addLine(Line(-2ll * points[1].second, points[1].second * points[1].second + del, 0));
     for(ll i = 1; i < points.size(); i ++) {
-        auto best = tree.get(points[i].first, &(tree.root));
-        dp[i] = best.y(points[i].first) + points[i].first * points[i].first;
-        delta[i] = best.delta + 1;
+        auto best = lc.get(points[i].first);
+        dp[i] = best.first + points[i].first * points[i].first;
+        delta[i] = best.second + 1;
         if(i != points.size() - 1) {
-            tree.add(Line(-2ll * points[i + 1].second, dp[i] - toSqr(max(points[i].first - points[i + 1].second, 0ll)) + toSqr(points[i + 1].second) + del, delta[i]), &(tree.root));
+            lc.addLine(Line(-2ll * points[i + 1].second, dp[i] - toSqr(max(points[i].first - points[i + 1].second, 0ll)) + toSqr(points[i + 1].second) + del, delta[i]));
         }
+
     }
     return delta[points.size() - 1];
 }
@@ -128,7 +106,7 @@ long long take_photos(int N, int M, int K, std::vector<int> r, std::vector<int> 
     reverse(points.begin(), points.end());
     ld lft = -mod, rght = mod;
 
-    for(int iter = 0; iter < 50; iter ++) {
+    for(int iter = 0; iter < 60; iter ++) {
         ld m = (lft + rght) / 2.;
         if(calculate(m) > k) {
             lft = m;
